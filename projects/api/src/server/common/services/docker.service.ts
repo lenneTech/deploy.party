a113834eca9169a3bf42f3c473850072509381b8
Pascal Klesse <pascal.klesse@lenne.tech>
2025-03-10 23:57:32 +0100

    fix: format environment variable assignment in docker service for better readability

--- projects/api/src/server/common/services/docker.service.ts
+++ projects/api/src/server/common/services/docker.service.ts
@@ -92,26 +92,26 @@ export class DockerService {
           const key = Array.from({length: 36}, () => Math.floor(Math.random() * 16).toString(16)).join('');
           const secret = Array.from({length: 36}, () => Math.floor(Math.random() * 16).toString(16)).join('');
           container.env = `
-                KEY=${key}
-                SECRET=${secret}
+          KEY=${key}
+          SECRET=${secret}
 
-                DB_CLIENT=pg
-                DB_HOST=database
-                DB_PORT=5432
-                DB_DATABASE=directus
-                DB_USER=directus
-                DB_PASSWORD=directus
+          DB_CLIENT=pg
+          DB_HOST=database
+          DB_PORT=5432
+          DB_DATABASE=directus
+          DB_USER=directus
+          DB_PASSWORD=directus
 
-                CACHE_ENABLED=true
-                CACHE_STORE=redis
-                REDIS=redis://cache:6379
+          CACHE_ENABLED=true
+          CACHE_STORE=redis
+          REDIS=redis://cache:6379
 
-                ADMIN_EMAIL=admin@directus.com
-                ADMIN_PASSWORD=d1r3ctu5
+          ADMIN_EMAIL=admin@directus.com
+          ADMIN_PASSWORD=d1r3ctu5
 
-                # Make sure to set this in production
-                # (see https://docs.directus.io/self-hosted/config-options#general)
-                PUBLIC_URL=https://${container.url}
+          # Make sure to set this in production
+          # (see https://docs.directus.io/self-hosted/config-options#general)
+          PUBLIC_URL=https://${container.url}
           `;
           await this.containerService.updateForce(container.id, {env: container.env});
           await this.createEnvFile(container);
